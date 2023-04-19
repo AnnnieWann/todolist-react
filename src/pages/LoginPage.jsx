e87a19387eb5fc4d28a25c02b3e3dead49943403
Annie <annie_wan1209@hotmail.com>
2023-04-19 16:50:37 +0800

    feat: add props and update ui of signup and login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,26 +4,46 @@ import {
   AuthButton,
   AuthLinkText,
 } from 'components/common/auth.styled';
+
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const LoginPage = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <AuthContainer>
       <div>
         <ACLogoIcon />
       </div>
-      <h1>登入 Todo</h1>
+      <h1>Login Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Username"
+          value={userName}
+          placeholder="Please Enter Your Username"
+          onChange={(userNameInputValue) => setUserName(userNameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Password"
+          type="password"
+          value={password}
+          placeholder="Please Enter Your Password"
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
-      <AuthLinkText>註冊</AuthLinkText>
+      <AuthButton>Login</AuthButton>
+      <Link to="/signup">
+        <AuthLinkText>Register</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -4,30 +4,56 @@ import {
   AuthButton,
   AuthLinkText,
 } from 'components/common/auth.styled';
+
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const SignUpPage = () => {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <AuthContainer>
       <div>
         <ACLogoIcon />
       </div>
-      <h1>建立您的帳號</h1>
+      <h1>Create Your Account</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Username"
+          value={userName}
+          placeholder="Please Enter Your Username"
+          onChange={(userNameInputValue) => setUserName(userNameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Email"
+          value={email}
+          placeholder="Please Enter Your Email"
+          onChange={(emailInputValue) => setEmail(emailInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Password"
+          type="password"
+          value={password}
+          placeholder="Please Enter Your Password"
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
-      <AuthLinkText>取消</AuthLinkText>
+      <AuthButton>Register</AuthButton>
+      <Link to="/login">
+        <AuthLinkText>Cancel</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
